779454b4a07b0321a2f2c8387abfd4429d8dc4e1
Adithya Sapalya <adithyasapalya30@gmail.com>
2026-01-29 20:15:11 +0530

    initial commit

---
+++ yvd.py
@@ -0,0 +1,14 @@
+from pytube import YouTube
+import tkinter as tk
+from tkinter import filedialog 
+
+def download_video(url, save_path):
+    try:
+        yt = YouTube(url)
+        streams = yt.streams.filter(progressive= True, file_extension = "mp4")
+
+
+
+    except Exception as e:
+        print(e)
+    
